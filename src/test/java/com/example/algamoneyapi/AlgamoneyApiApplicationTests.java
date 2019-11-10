fdda3000847c2b49f61f6a646a6d1551c6f0d1fc
Aloisio Vieira <aloisio.vieira@gmail.com>
2019-11-10 18:53:03 -0300

    convertido para junit

--- src/test/java/com/example/algamoneyapi/AlgamoneyApiApplicationTests.java
+++ src/test/java/com/example/algamoneyapi/AlgamoneyApiApplicationTests.java
@@ -1,12 +1,14 @@
 package com.example.algamoneyapi;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import com.example.algamoney.api.AlgamoneyApiApplication;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = AlgamoneyApiApplication.class)
 public class AlgamoneyApiApplicationTests {
 
 	@Test
