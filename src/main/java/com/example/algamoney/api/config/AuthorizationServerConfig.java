ddeb34ef869287d63dd8c835890c77b1fd3ccd50
Aloisio Vieira <aloisio.vieira@gmail.com>
2019-11-10 23:48:33 -0300

    alternando oauth2 e basic security com profiles

--- src/main/java/com/example/algamoney/api/config/AuthorizationServerConfig.java
+++ src/main/java/com/example/algamoney/api/config/AuthorizationServerConfig.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
@@ -18,6 +19,7 @@ import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenCo
 
 import com.example.algamoney.api.config.token.CustomTokenEnhancer;
 
+@Profile("oauth-security")
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
@@ -27,11 +29,15 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-		clients.inMemory().withClient("angular").secret("@ngul@r0").scopes("read", "write")
-				.authorizedGrantTypes("password", "refresh_token").accessTokenValiditySeconds(1800)
-				.refreshTokenValiditySeconds(3600 * 24).and().withClient("mobile").secret("m0b1l30").scopes("read")
-				.authorizedGrantTypes("password", "refresh_token").accessTokenValiditySeconds(1800)
-				.refreshTokenValiditySeconds(3600 * 24);
+		clients
+			.inMemory()
+			.withClient("angular")
+			.secret("@ngul@r0")
+			.scopes("read", "write")
+			.authorizedGrantTypes("password", "refresh_token").accessTokenValiditySeconds(1800)
+			.refreshTokenValiditySeconds(3600 * 24).and().withClient("mobile").secret("m0b1l30").scopes("read")
+			.authorizedGrantTypes("password", "refresh_token").accessTokenValiditySeconds(1800)
+			.refreshTokenValiditySeconds(3600 * 24);
 	}
 
 	@Override
